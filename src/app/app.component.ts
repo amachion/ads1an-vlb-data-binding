506f5635c669986bcfc5384fbae3afa0b227d1b5
Andreia <andreia.machion@saojudas.br>
2021-10-01 08:33:37 -0300

    até listagem 2.4.5 apotila 08 angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding';
+  nome = "Andreia";
+  idade = 20;
+
+  lancaDado() {
+    return Math.floor(Math.random() * 6 + 1);
+  }
+
 }
